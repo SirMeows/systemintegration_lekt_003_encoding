6f69ba9c94041c291b93547467f7ff37c4fae52e
SirMeows <59692332+SirMeows@users.noreply.github.com>
2024-02-08 12:42:44 +0100

    initial code snippets from class, incomplete

---
+++ node/index.js
@@ -0,0 +1,6 @@
+const base64EncodedString = btoa("hello there, this is base 64 encoded");
+const base64DecodedString = atob("hello there, this is base 64 decoded");
+
+console.log(base64DecodedString);
+
+
---
+++ python/main.py
@@ -0,0 +1,4 @@
+encoded_string = "hallå".encode()
+print(type(encoded_string))
+decoded_string = encoded_string.decode()
+print(decoded_string)
